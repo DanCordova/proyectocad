e1c3d9f7174ed0da3b2f4cd50de83597eda10c02
danbermudez <a01377242@itesm.mx>
2021-11-04 21:16:28 -0600

    Error_Table Script

--- conexion.php
+++ conexion.php
@@ -2,14 +2,14 @@
 class Conexion
     public static function Conectar()   {
         define('servidor','localhost');
-        define('Test','proyectocad ');
+        define('nombrebd','proyectocad ');
         define('usuario','root');
         define('password','password');
 
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND -> "SET NAMES utf-8");
 
         try{
-            $conexion = new PDO("mysql:host=".servidor"; dbname=".Test,usuario,password,$opciones);
+            $conexion = new PDO("mysql:host=".servidor"; dbname=".nombrebd,usuario,password,$opciones);
             return $conexion
         } catch(Exception $e){
             die("Error de conxión es".$e -> getMessage());
@@ -21,7 +21,5 @@ class Conexion
 
 
 }
-
-
 
 >
