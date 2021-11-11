15a34e6c8bd6644a93bbc9d7ed3025675d601527
danbermudez <a01377242@itesm.mx>
2021-11-10 19:01:09 -0600

    Tablas2

---
+++ api4.php
@@ -0,0 +1,56 @@
+<?php
+    require_once("connect4.php");
+    if ((isset($_GET["o"])) || isset($_POST["action"])) {
+        if (isset($_GET["o"])) {
+            $operation = $_GET["o"];
+            switch($operation) {
+                case "test":
+                    echo "Test";
+                    break;
+                default:
+                    echo "400 - Bad Request";
+                    break;
+            }
+        }
+    
+        if (isset($_POST["action"])) {
+            $action = $_POST["action"];
+            switch($action) {
+                case "newIrrigation":
+                    $ph = $_POST["ph"];
+                    $ce = $_POST["ce"];
+                    $volume1 = $_POST["v1"];
+                    $volume2 = $_POST["v2"];
+                    $type = $_POST["t"];
+                    $form = $_POST["f"];
+                    $irrigationId = $dbConnection->Insert(
+                        "INSERT INTO irrigationsdrains(ph, ce, volume1, volume2, type, form) values(?, ?, ?, ?, ?, ?)",
+                        [
+                            "ssssii", $ph, $ce, $volume1, $volume2, $type, $form
+                        ]
+                    );
+                    $irrigation = new stdClass();
+                    $irrigation->Id = $irrigationId;
+                    echo json_encode($irrigation);
+                    break;
+                case "getIrrigationData":
+                    $start = isset($_POST["start"]) ? $_POST["start"] : 0;
+                    $length = isset($_POST["length"]) ? $_POST["length"] : 10;
+                    $recordsTotal = $dbConnection->Select("SELECT COUNT(*) AS 'recordsTotal' FROM irrigationsdrains")[0]["recordsTotal"];
+                    $data = $dbConnection->Select("SELECT * FROM irrigationsdrains i ORDER BY i.id LIMIT ".$start.",".$length);
+                    $response = new stdClass();
+                    $response->draw = isset($_POST["draw"]) ? $_POST["draw"] : 1;
+                    $response->recordsTotal = isset($_POST["length"]) ? $_POST["length"] : 1;
+                    $response->recordsFiltered = intval($recordsTotal);
+                    $response->data = $data;
+                    echo json_encode($response);
+                    break;
+                default:
+                    echo "400 - Bad Request";
+                    break;
+            }
+        }
+    } else {
+        die("401 - Unauthorized");
+    }
+?>
---
+++ connect4.php
@@ -0,0 +1,8 @@
+<?php
+    include "DatabaseConnector4.php";
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "password";
+    $dbName = "proyectocad4";
+    $dbConnection = new DatabaseConnector($host, $dbName, $dbUsername, $dbPassword);
+?>
