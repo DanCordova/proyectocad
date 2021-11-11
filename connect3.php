5fc4619f75277c7dc9cefaa1e70d2d834b470508
danbermudez <a01377242@itesm.mx>
2021-11-10 18:56:09 -0600

    Invernadero

---
+++ connect3.php
@@ -0,0 +1,8 @@
+<?php
+    include "DatabaseConnector3.php";
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "password";
+    $dbName = "proyectocad3";
+    $dbConnection = new DatabaseConnector($host, $dbName, $dbUsername, $dbPassword);
+?>
