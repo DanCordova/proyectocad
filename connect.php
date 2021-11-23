e2ace42a5376c4c83affd79d81e56c6905a1a876
danbermudez <a01377242@itesm.mx>
2021-11-23 17:00:51 -0600

    Comment

--- connect.php
+++ connect.php
@@ -1,8 +1,13 @@
-<?php
-    include "DatabaseConnector.php";
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "password";
-    $dbName = "proyectocad";
-    $dbConnection = new DatabaseConnector($host, $dbName, $dbUsername, $dbPassword);
-?>
+
+
+    <!-- Codigo conexion con la base de datos mediante php -->
+    <?php
+        // El codigo php incluye las variables para acceder al servidor SQL remoto
+        // Databaseconnector es incluido y se iniciliza la funcion DatabaseConnector dentro de Databaseconnector.php
+        include "DatabaseConnector.php";    
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "password";
+        $dbName = "proyectocad";
+        $dbConnection = new DatabaseConnector($host, $dbName, $dbUsername, $dbPassword);
+    ?>
