edc895753832e00846eecec6cce9d91f94f0bd62
danbermudez <a01377242@itesm.mx>
2021-11-28 18:29:20 -0600

    Error Database1

--- connect.php
+++ connect.php
@@ -2,8 +2,7 @@
 
     <!-- Codigo conexion con la base de datos mediante php -->
     <?php
-        // El codigo php incluye las variables para acceder al servidor SQL remoto
-        // Databaseconnector es incluido y se iniciliza la funcion DatabaseConnector dentro de Databaseconnector.php
+        
         include "DatabaseConnector.php";    
         $host = "localhost";
         $dbUsername = "root";
